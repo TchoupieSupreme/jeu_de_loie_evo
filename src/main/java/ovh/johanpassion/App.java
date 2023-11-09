11eaaa9531f16fb19feeb87aba5ab3bf2ae57d39
TchoupieSupreme <passion.johan@gmail.com>
2023-11-09 09:14:55 +0100

    different message

--- src/main/java/ovh/johanpassion/App.java
+++ src/main/java/ovh/johanpassion/App.java
@@ -15,7 +15,13 @@ public class App {
                 clavier.nextLine();
                 int lancer = generateur.nextInt(6)+1;
                 caseActuelle += lancer;
-                System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (encore "+(20-caseActuelle)+")");
+                if (20-caseActuelle>0) {
+                    System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (encore "+(20-caseActuelle)+")");
+                }else if(20-caseActuelle==0){
+                    System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" !");
+                }else{
+                    System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (vous avez dépassé de "+(Math.abs(20-caseActuelle))+" case)");
+                }
             }
             if (20-caseActuelle !=0) {
                 System.out.println("Vous avez perdu !");
