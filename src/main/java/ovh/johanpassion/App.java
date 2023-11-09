d6179238a4e543b18fd7857e1f79df3dbdff395a
TchoupieSupreme <passion.johan@gmail.com>
2023-11-09 10:05:57 +0100

    1parti

--- src/main/java/ovh/johanpassion/App.java
+++ src/main/java/ovh/johanpassion/App.java
@@ -9,10 +9,12 @@ public class App {
         Random generateur = new Random();
         int caseActuelle = 0;
         int nbPartie = 1;
+        int i=0;
         Scanner clavier = new Scanner(System.in);
         do {
             caseActuelle =0;
-            for (int i =0;i<5;i++){     
+            while (caseActuelle!=20){  
+                i++;   
                 int lancer = generateur.nextInt(6)+1;
                 caseActuelle += lancer;
                 if (20-caseActuelle>0) {
@@ -20,7 +22,8 @@ public class App {
                 }else if(20-caseActuelle==0){
                     System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" !");
                 }else{
-                    System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (vous avez dépassé de "+(Math.abs(20-caseActuelle))+" case)");
+                    System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (vous avez dépassé de "+(Math.abs(20-caseActuelle))+" case donc vous retourner sur la case "+(20-(Math.abs(20-caseActuelle)))+")");
+                    caseActuelle = 20-(Math.abs(20-caseActuelle));
                 }
             }
             if (20-caseActuelle !=0) {
